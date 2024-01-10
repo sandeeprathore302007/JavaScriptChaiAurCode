3afb8787bc7c95c54dc0f9312bc1d66244f0d12e
sandeeprathore302007 <sandeeprathore302007@gmail.com>
2024-01-10 08:12:58 +0530

    Currency Convertor project added but rendering issue  still be debugg

---
+++ 06currencyConverter/src/App.jsx
@@ -0,0 +1,103 @@
+import { useState } from 'react'
+import { InputBox } from './components';
+import { useCurrencyInfo } from './hooks/useCurrencyinfo'
+import './App.css'
+ 
+ 
+
+function App() {
+
+   const [amount , setAmount] = useState(0);
+   const[to, setTo ] = useState("usd");
+   const[from , setFrom ] = useState("inr");
+   const[convertedAmount , setConverteddAmount] = useState(0);
+
+   const currencyInfo  = useCurrencyInfo(from);
+
+   const options = Object.keys(currencyInfo);
+
+
+   const swap = ()=>
+   {
+    setTo(from)
+    setFrom(to)
+    setConverteddAmount(amount)
+    setAmount(convertedAmount)
+   }
+
+   const convert = ()=>
+   {
+    setConverteddAmount(amount*currencyInfo[to])
+   }
+
+   return (
+    <>
+       <h1>currency Converter</h1>
+      <div
+            className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+            style={{
+                backgroundImage: `url('https://thumbs.dreamstime.com/b/global-currency-technology-background-money-transfer-stock-market-concept-money-transfer-global-currency-stock-exchange-153251024.jpg')`,
+            }}
+        >
+            <div className="w-full">
+                <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+                    <form
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            convert();
+                           
+                        }}
+                    >
+                        <div className="w-full mb-1">
+                            <InputBox
+                                label="From"
+                                amount={amount}
+                                currencyOptions={options}
+                                onCurrencyChange={(currency)=>
+                                {
+                                  setAmount(amount);
+                                  
+                                }}
+                                selectCurrency= {from}
+                                onAmountChange={(amount)=>
+                                {
+                                  setAmount(amount);
+                                }}
+                                
+                            />
+                        </div>
+                        <div className="relative w-full h-0.5">
+                            <button
+                                type="button"
+                                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                                
+                           onClick={swap} >
+                                swap
+                            </button>
+                        </div>
+                        <div className="w-full mt-1 mb-4">
+                            <InputBox
+                                label="To"
+                                amount={convertedAmount}
+                                currencyOptions={options}
+                                 onCurrencyChange={(currency)=>
+                                {
+                                     setTo(currency);
+                                }}
+                                selectCurrency= {from}
+                                amountDisable
+                                
+                            />
+                        </div>
+                        <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
+                            Convert {from.toUpperCase()} to {to.toUpperCase()}
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default App
